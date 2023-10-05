73cee717a6aa61f842c06c1f883525c843ff5c26
G Locke <jkeys726@gmail.com>
2023-10-04 20:18:48 -0500

    docstring typo

--- Project 2/first_and_last_largest.py
+++ Project 2/first_and_last_largest.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def firstAndLastLargest(self, list):
-        """Takes a list of n integerns and determines the first and last occurence of the largest element in the list."""
+        """Takes a list of n integers and determines the first and last occurence of the largest element in the list."""
         max_value = max(list)
         occurrences = [i for i, x in enumerate(list) if x == max_value]
         return [min(occurrences), max(occurrences)]
